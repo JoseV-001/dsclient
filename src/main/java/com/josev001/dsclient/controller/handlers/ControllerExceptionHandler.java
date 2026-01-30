d3917ca0632eb3e7a6b937ad169b5073658c0410
JoseV-001 <Leaphaz@gmail.com>
2026-01-29 21:23:17 -0300

    Add DTO validation

--- src/main/java/com/josev001/dsclient/controller/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/josev001/dsclient/controller/handlers/ControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.josev001.dsclient.controller.handlers;
 
 import com.josev001.dsclient.dto.CustomError;
+import com.josev001.dsclient.dto.ValidationError;
 import com.josev001.dsclient.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +20,17 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(),f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 
--- src/main/java/com/josev001/dsclient/dto/ClientDTO.java
+++ src/main/java/com/josev001/dsclient/dto/ClientDTO.java
@@ -1,18 +1,31 @@
 package com.josev001.dsclient.dto;
 
 import com.josev001.dsclient.entities.Client;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
 public class ClientDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message =  "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message =  "Campo requerido")
     private String name;
+
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter exatamente 11 dígitos numéricos")
+    @NotBlank(message =  "Campo requerido")
     private String cpf;
+
+
+    @PositiveOrZero(message = "Renda não pode ser negativa")
+    @NotNull(message = "Campo requerido")
     private Double income;
+
+    @PastOrPresent(message = "Data de nascimento não pode ser futura")
     private LocalDate birthDate;
+
+    @PositiveOrZero(message = "A quantidade de filhos não pode ser negativa")
     private int children;
 
     public ClientDTO() {
